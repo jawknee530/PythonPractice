5cc78c518376c50a27d8ca2b09b5cf21dc37f0d7
Jon Allen <jallen59@mail.csuchico.edu>
2017-01-25 23:01:57 -0800

    solved

--- graphNav.py
+++ graphNav.py
@@ -1,41 +1,28 @@
 def nav_graph(graph, cur_row, cur_col, directions, weight):
-    right_node = []
-    below_node = []
-    while cur_row < len(graph) or cur_col < len(graph):
-        if cur_row < len(graph)-1:
-            directions += 'd,'
-            weight += int(graph[cur_row+1][cur_col])
-            print('going down')
-            below_node = nav_graph(graph, cur_row+1, cur_col, directions, weight)
-            print('gone down')
-            # below_node = int(graph[cur_row+1][cur_col])
-        else:
-            below_node = (0, 0, 0, 0, float('-inf'))
+    if cur_row < len(graph)-1:
+        below_weight = weight + int(graph[cur_row+1][cur_col])
+        below_node = nav_graph(graph, cur_row+1, cur_col, directions+'d,', below_weight)
+    else:
+        below_node = [directions, weight]
 
-        if cur_col < len(graph)-1:
-            directions += 'r,'
-            weight += int(graph[cur_row][cur_col+1])
-            print('going right')
-            right_node = nav_graph(graph, cur_row, cur_col+1, directions, weight)
-            print('gone right')
-            # right_node = int(graph[cur_row][cur_col+1])
-        else:
-            right_node = (0, 0, 0, 0, float('-inf'))
+    if cur_col < len(graph)-1:
+        right_weight = weight + int(graph[cur_row][cur_col+1])
+        right_node = nav_graph(graph, cur_row, cur_col+1, directions+'r,', right_weight)
+    else:
+        right_node = [directions, weight]
 
-        if right_node[1] > below_node[1]:
-            return right_node
-        else:
-            return below_node
     if right_node[1] > below_node[1]:
         return [right_node[0], right_node[1]]
     else:
         return [below_node[0], below_node[1]]
+
 
 def parse_graph(str):
     rows = str.split(';')
     graph = []
     for row in rows:
         graph.append(row.split(','))
+    print('Grid:')
     for row in graph:
         for col in row:
             print(col, end=' ')
@@ -43,10 +30,8 @@ def parse_graph(str):
     return nav_graph(graph, 0, 0, '', int(graph[0][0]))
 
 
-
-# graph = input("""Input a string representing a graph.
-# The format is row deliminated by ';'
-# and column deliminated by ','""")
-
+print('Find the highest weighted path through a grid of numbers')
 results = parse_graph('1,2,3;6,5,4;8,-7,9')
+results[0] = results[0][:-1]
+print('Results:')
 print(results)
